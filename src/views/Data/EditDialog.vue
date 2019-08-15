b89852907c8e56683d54193ba48e44daea3653ef
wozien <571455773@qq.com>
2019-08-15 23:30:43 +0800

    ‘增加课程编辑dialog

---
+++ src/views/Data/EditDialog.vue
@@ -0,0 +1,64 @@
+<template>
+  <el-dialog title="编辑课程" :visible.sync="dialogVisible" :close-on-click-modal="false" :show-close="false">
+    <el-form ref="ruleForm" :model="form" label-width="100px" size="small" class="form-box">
+      <el-form-item prop="title" label="课程名称">
+        <el-input v-model="form.title" placeholder="请输入课程名称"></el-input>
+      </el-form-item>
+      <el-form-item prop="level" label="课程等级">
+        <el-select v-model="form.level" placeholder="请选择课程等级">
+          <el-option label="初级" value="初级"></el-option>
+          <el-option label="中级" value="中级"></el-option>
+          <el-option label="高级" value="高级"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item prop="count" label="报名人数">
+        <el-input v-model="form.count" placeholder="请输入报名人数"></el-input>
+      </el-form-item>
+      <el-form-item prop="date" label="上线时间">
+        <el-date-picker v-model="form.date" type="date" placeholder="选择日期" value-format="yyyy-MM-dd"></el-date-picker>
+      </el-form-item>
+      <el-form-item prop="type" label="技术栈">
+        <el-radio-group v-model="form.type">
+          <el-radio label="vue" name="type"></el-radio>
+          <el-radio label="react" name="type"></el-radio>
+          <el-radio label="node" name="type"></el-radio>
+          <el-radio label="小程序" name="type"></el-radio>
+          <el-radio label="angular" name="type"></el-radio>
+        </el-radio-group>
+      </el-form-item>
+    </el-form>
+    <div slot="footer">
+      <el-button size="mini" @click="$emit('close-dialog')">取消</el-button>
+      <el-button size="mini" type="primary">确定 </el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Provide, Prop } from 'vue-property-decorator';
+
+@Component({
+  components: {}
+})
+export default class EditDialog extends Vue {
+  // 属性或参数中使用 ！：表示强制解析（告诉typescript编译器，这里一定有值），常用于vue-decorator中的@Prop
+  @Prop(Boolean) dialogVisible!: boolean;
+  @Prop(Object) form!: {
+    _id: string;
+    title: string;
+    type: string;
+    level: string;
+    count: string;
+    date: string;
+  };
+}
+</script>
+
+<style lang="scss" scoped>
+.form-box {
+  .el-input,
+  .el-select {
+    width: 200px !important;
+  }
+}
+</style>
--- src/views/Data/TableData.vue
+++ src/views/Data/TableData.vue
@@ -2,7 +2,7 @@
   <div class="table-data">
     <div class="search-box">
       <el-input size="small" v-model="searchVal" placeholder="请输入课程名称检索"></el-input>
-      <el-button type="primary" size="small" icon="el-icon-search">搜索</el-button>
+      <el-button type="primary" size="small" icon="el-icon-search" @click="handleSearch">搜索</el-button>
     </div>
     <el-table :data="tableData" border style="width:100%" :height="tHeight" class="table-box">
       <el-table-column type="index" label="序号" width="60"></el-table-column>
@@ -12,24 +12,35 @@
       <el-table-column prop="count" label="报名人数" width="120"></el-table-column>
       <el-table-column prop="date" label="上线日期" width="160"></el-table-column>
       <el-table-column label="操作" width="160">
-        <template>
-          <el-button size="mini">编辑</el-button>
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
           <el-button size="mini" type="danger">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
     <!-- 分页 -->
     <div class="pages">
-      <el-pagination layout="total,sizes,prev,pager,next,jumper" :page-sizes="[5, 10, 20]" :page-size="size" :total="total"> </el-pagination>
+      <el-pagination
+        layout="total,sizes,prev,pager,next,jumper"
+        :page-sizes="[5, 10, 20]"
+        :page-size="size"
+        :total="total"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      >
+      </el-pagination>
     </div>
+
+    <edit-dialog :dialog-visible="dialogVisible" :form="formData" @close-dialog="closeDialog"></edit-dialog>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Provide } from 'vue-property-decorator';
+import EditDialog from './EditDialog.vue';
 
 @Component({
-  components: {}
+  components: { EditDialog }
 })
 export default class TableData extends Vue {
   @Provide() searchVal: string = '';
@@ -39,16 +50,51 @@ export default class TableData extends Vue {
   @Provide() total: number = 0;
   @Provide() tHeight: number = document.body.offsetHeight - 270;
 
+  @Provide() dialogVisible: boolean = false;
+  @Provide() formData: object = {};
+
   created() {
-    this.initData();
+    this.loadData();
   }
 
-  initData() {
+  loadData() {
     (this as any).$axios.get(`/api/profiles/loadMore/${this.page}/${this.size}`).then((res: any) => {
       // console.log(res);
       this.tableData = res.data.data.list;
       this.total = res.data.data.total;
     });
+  }
+
+  loadSearchData() {
+    (this as any).$axios.get(`/api/profiles/search/${this.searchVal}/${this.page}/${this.size}`).then((res: any) => {
+      this.tableData = res.data.datas.list;
+      this.total = res.data.datas.total;
+    });
+  }
+
+  handleSizeChange(val: number) {
+    this.size = val;
+    this.page = 1;
+    this.searchVal ? this.loadSearchData() : this.loadData();
+  }
+
+  handleCurrentChange(val: number) {
+    this.page = val;
+    this.searchVal ? this.loadSearchData() : this.loadData();
+  }
+
+  handleSearch() {
+    this.page = 1;
+    this.searchVal ? this.loadSearchData() : this.loadData();
+  }
+
+  handleEdit(index: number, row: any) {
+    this.formData = row;
+    this.dialogVisible = true;
+  }
+
+  closeDialog() {
+    this.dialogVisible = false;
   }
 }
 </script>
